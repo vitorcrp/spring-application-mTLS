d04201aca33c2df2f086c43dd63e76f3544578b4
vitorcrp <66389015+vitorcrp@users.noreply.github.com>
2022-11-11 18:40:15 -0300

    start project

---
+++ webhookserver/webhookserver/src/main/java/com/mtlsapplication/webhookserver/WebhookserverApplication.java
@@ -0,0 +1,13 @@
+package com.mtlsapplication.webhookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookserverApplication.class, args);
+	}
+
+}
---
+++ webhookserver/webhookserver/src/test/java/com/mtlsapplication/webhookserver/WebhookserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtlsapplication.webhookserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
