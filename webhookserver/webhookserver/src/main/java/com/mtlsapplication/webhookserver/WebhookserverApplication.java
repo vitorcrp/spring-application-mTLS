85ce64b7ec676c95e0d7a5b7d3093ce07a9be66e
vitorcrp <vitorcrp@LAPTOP-33UHKRVK.localdomain>
2022-11-14 15:59:42 -0300

    implementacao do mTLS

--- webhookserver/webhookserver/src/main/java/com/mtlsapplication/webhookserver/WebhookserverApplication.java
+++ webhookserver/webhookserver/src/main/java/com/mtlsapplication/webhookserver/WebhookserverApplication.java
@@ -2,12 +2,19 @@ package com.mtlsapplication.webhookserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebhookserverApplication {
+public class WebhookserverApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebhookserverApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WebhookserverApplication.class);
 	}
 
 }
